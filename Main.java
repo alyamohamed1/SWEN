fd87b9ada9797d6b2576f82c0ab9b493267ee27f
alyamohamed1 <alya.altawash@gmail.com>
2025-09-30 16:22:53 +0300

    created main class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🌠 Welcome to the Space Quiz Game!");
+        Quiz quiz = new Quiz();
+        quiz.start();
+    }
+}
