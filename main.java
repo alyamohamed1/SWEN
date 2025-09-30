69f06a11581a16888222cb3ca692334b6e557065
alyamohamed1 <alya.altawash@gmail.com>
2025-09-30 16:01:37 +0300

    removed main class to push from new branch

--- main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("🌠 Welcome to the Space Quiz Game!");
-        Quiz quiz = new Quiz();
-        quiz.start();
-    }
-}
